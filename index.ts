f74011c2538cf687a135b31b66036a8bbcfa1a47
father1971 <rotanovav@gmail.com>
2024-02-20 23:34:33 +0300

    “Project base done

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { createClient } from 'redis';
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.get('/', function (request, response) {
+  response.send('Я живой!');
+});
+const port = process.env.PORT;
+app.listen(port, function () {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
