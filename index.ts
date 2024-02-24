e8c1a875e86cb85154ee5c02407d02d102deee75
father1971 <rotanovav@gmail.com>
2024-02-24 22:20:45 +0300

    Добавлен путь GET

--- index.ts
+++ index.ts
@@ -3,12 +3,26 @@ import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { createClient } from 'redis';
+import { Task } from './commonTypes';
 
 dotenv.config();
 const app = express();
 
 const redisClient = createClient();
 const connection = redisClient.connect();
+
+app.get('/tasks/', async function (request, response) {
+  const redis = await connection;
+  const savedTasks = await redis.get('tasks');
+
+  if (savedTasks === null) {
+    response.send([]);
+    return;
+  }
+
+  const parsedTasks: Task[] = JSON.parse(savedTasks);
+  response.send(parsedTasks);
+});
 
 app.use(cors());
 app.use(bodyParser.json());
